1f9cb0481b244147932f8e0cac524aab929c7535
zhongtao <1363517977@qq.com>
2020-01-16 14:06:15 +0800

    引入了mint-ui

--- cdq/src/main.js
+++ cdq/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//1:引入mint-ui组件库中所有组件
+import MintUI from "mint-ui"
+console.log(MintUI);
+//2.单独引入mint-ui组件库中样式文件
+import "mint-ui/lib/style.css"
+//3.注册MintUI对象
+Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
